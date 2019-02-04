da0b35ffbf0cecc6e0be2651a93cb5e346940c6c
Emilio Garcia <egarcia@redhat.com>
2019-02-04 15:23:34 -0500

    added output option and the ability to select sorting key

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # SortYamlFiles
-This tool lets you sort kubernetes yaml files by metadata.name. Thats it.
+This tool lets you sort kubernetes yaml files by the key of your choosing.
 
-## Usage
-Make sure you have python 2 and pyYaml. It can be installed like this:
+## Pre-Reqs
+Make sure you have python 2 and pyYaml.
+PYYaml can be installed like this:
+
 ```bash
 pip install --user pyYaml
 ```
 
-The file can be run as follows
-```bash
-./sortYaml <path to your file>
+## Usage
+When you run the file, it will print the results to standard out. By default, it will sort each file by its metadata.name. However, you may set the `--key` option to change this behavior.
 
-it will print the results to stdout
+```
+usage: ./sortYaml.py [-h] [--key KEY] [--out OUT] fileName
+
+positional arguments:
+  fileName    the path to the file that you want to sort
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --key KEY   This changes the key you want to sort on from metadata.name to
+              whatever you choose, as long as its a valid key. Please note
+              that if you enter an invalid key, it will simply not sort the
+              file.
+  --out OUT   Write to a file rather than print in standard out. This will
+              overwrite existing files with the same name.
+```
--- sortYaml.py
+++ sortYaml.py
@@ -3,6 +3,30 @@ import argparse
 import yaml
 
 
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "fileName",
+        help="the path to the file that you want to sort"
+    )
+    parser.add_argument(
+        "--key",
+        default="metadata.name",
+        help="This changes the key you want to sort on from " +
+        "metadata.name to whatever you choose, as long as its a valid key." +
+        " Please note that if you enter an invalid key, it will simply" +
+        " not sort the file."
+    )
+    parser.add_argument(
+        "--out",
+        default="",
+        help="Write to a file rather than print in standard out. This will " +
+        "overwrite existing files with the same name."
+    )
+    args = parser.parse_args()
+    return args.fileName, args.key, args.out
+
+
 def splitIntoFiles(fileName):
     yaml = ""
     with open(fileName) as file:
@@ -10,19 +34,30 @@ def splitIntoFiles(fileName):
     return yaml.split("---")
 
 
-def mapByName(files):
+def keyVal(data, key):
+    key = key.split(".")
+    check = data
+    for i in range(len(key)):
+        if i > 0:
+            check = check[key[i-1]]
+        if key[i] not in check:
+            return False, ""
+
+    return True, check[key[-1]]
+
+
+def mapByKey(files, key):
     fileMap = {}
     countUnnamed = 0
     for file in files:
-        data = yaml.load(file)
-        name = ""
-        if "metadata" in data:
-            if "name" in data["metadata"]:
-                name = data["metadata"]["name"]
-        if name == "":
-            name = str(countUnnamed)
-            countUnnamed += 1
-        fileMap[name] = file
+        if file != None and file != "\n":
+            data = yaml.load(file)
+            hasKey, keyName = keyVal(data, key)
+            if hasKey:
+                fileMap[keyName] = file
+            else:
+                fileMap[str(countUnnamed)] = file
+                countUnnamed += 1
     return fileMap
 
 
@@ -35,21 +70,21 @@ def sortAndDumpYaml(fileMap):
             outfile = fileMap[key]
             first = False
         else:
-            outfile = outfile + "\n---\n" + fileMap[key]
+            outfile = outfile + "---" + fileMap[key]
     return outfile
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("fileName", help="the path to the file that you want to sort")
-    args = parser.parse_args()
-    fileName = args.fileName
+    fileName, key, out = getArgs()
     files = splitIntoFiles(fileName)
-    fileMap = mapByName(files)
+    fileMap = mapByKey(files, key)
     outfile = sortAndDumpYaml(fileMap)
-    print ""
-    print outfile
-
+    if out == "":
+        print outfile
+    else:
+        f = open(out, "w")
+        f.write(outfile)
+        f.close()
 
 if __name__ == "__main__":
     main()
